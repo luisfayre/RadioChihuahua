734a3e5d03509702bb8db3e14258ad47b33da071
luisfayre <luisfayre@hotmail.com>
2017-04-20 17:04:29 -0600

    Radio Funcionando

--- app/src/main/java/radiochihuahua/radiochihuahua/ReproductorActivity.java
+++ app/src/main/java/radiochihuahua/radiochihuahua/ReproductorActivity.java
@@ -1,8 +1,8 @@
 package radiochihuahua.radiochihuahua;
 
 
-import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,18 +10,27 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class ReproductorActivity extends AppCompatActivity {
 
-
+    //Texto
     private TextView ArtistatextView, CanciontextView, AlbumtextView;
+    //Botones
+    private ImageView play, siguiente, anterior, repetir, alternar;
+    //Imagen
     private View reproductor;
+    //Toolbar
     private Toolbar toolbar;
     private TextView TextView_toolbar;
+    //Reproductor
+    private MediaPlayer mediaPlayer;
+    private String STREAM_URL ="https://p-audio-4.radpog.com/play/15.mp3";
 
 
     @Override
@@ -29,15 +38,29 @@ public class ReproductorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor);
 
+
+        /**TEXTO-**/
         ArtistatextView = (TextView) findViewById(R.id.ArtistatextView);
         CanciontextView = (TextView) findViewById(R.id.CanciontextView);
         AlbumtextView = (TextView) findViewById(R.id.AlbumtextView);
         TextView_toolbar = (TextView) findViewById(R.id.TextView_toolbar);
-
+        //Fuente Letras
+        Typeface Bold = Typeface.createFromAsset(getAssets(), "Montserrat-Bold.otf");
+        Typeface Light = Typeface.createFromAsset(getAssets(), "Montserrat-Light.otf");
+        ArtistatextView.setTypeface(Bold);
+        CanciontextView.setTypeface(Bold);
+        AlbumtextView.setTypeface(Light);
+        TextView_toolbar.setTypeface(Bold);
+        /**BOTONES-*/
+        play = (ImageView) findViewById(R.id.imageView_rep_play);
+        anterior = (ImageView) findViewById(R.id.imageView_rep_anterior);
+        siguiente = (ImageView) findViewById(R.id.imageView_rep_siguiente);
+        alternar = (ImageView) findViewById(R.id.imageView_rep_shuffle);
+        repetir = (ImageView) findViewById(R.id.imageView_rep_repetir);
+        /**TOOLBAR-*/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.getBackground().setAlpha(0);
-
         if(getSupportActionBar() != null){
            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
            getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -45,12 +68,45 @@ public class ReproductorActivity extends AppCompatActivity {
 
         }
 
-        Typeface Bold = Typeface.createFromAsset(getAssets(), "Montserrat-Bold.otf");
-        Typeface Light = Typeface.createFromAsset(getAssets(), "Montserrat-Light.otf");
-        ArtistatextView.setTypeface(Bold);
-        CanciontextView.setTypeface(Bold);
-        AlbumtextView.setTypeface(Light);
-        TextView_toolbar.setTypeface(Bold);
+        /**REPRODUCTOR-*/
+        mediaPlayer = new MediaPlayer();
+
+
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                try{
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(STREAM_URL);
+                    mediaPlayer.prepareAsync();
+
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.
+                            OnPreparedListener(){
+                        @Override
+                        public void onPrepared(MediaPlayer mp){
+                            mp.start();
+
+                        }
+                    });
+
+                } catch (IOException e){
+                    e.printStackTrace();
+
+                }
+            }
+
+        });
+
+        repetir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mediaPlayer.stop();
+
+            }
+        });
+
+
 
 
     }
